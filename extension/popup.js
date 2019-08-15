3fd8ea513d3720ef60ed8250d7d6f67738b4c087
Liastre <liastre@outlook.com>
2019-08-15 23:52:10 +0300

    refactor

--- extension/popup.js
+++ extension/popup.js
@@ -1,12 +1,10 @@
 import Settings from "./settings.js"
 
 async function main() {
-    let switchAutofillSearch = document.querySelector("#switchAutofillSearch > input")
-
     // update settings
+    let switchAutofillSearch = document.querySelector("#switchAutofillSearch > input")
     let settings = await Settings.getInstance()
     switchAutofillSearch.checked = settings.autofillSearch
-
     switchAutofillSearch.addEventListener("change", (e) => {
         settings.autofillSearch = e.target.checked
     })
